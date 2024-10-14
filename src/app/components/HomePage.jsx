7e91b8a0b927f5f7f85702ad5f1adda0f0d4f9f7
= <=>
2024-10-14 18:38:16 +0800

    change img

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -2,7 +2,7 @@ export const HomeP = () => {
     return (
         <div className="mt-10">
             <section className="relative">
-                <img src="./largeImg.png" alt="" className="w-[1210px] h-[650px]" />
+                <img src="./largeImg.webp" alt="" className="w-[1210px] h-[650px]" />
                 <div className="bg-white border rounded-[15px] w-[600px] h-[250px] absolute bottom-0 left-0 mb-4 ml-4 p-[40px]">
                     <p className="text-white border rounded-[6px] bg-[#4B6BFB] w-[100px] px-[10px] py-[4px] text-[14px] font-[500] text-center">Technology</p>
                     <h1 className="text-[36px] font-sans leading-[40px] font-[600] pr-[70px] pt-[20px]">Grid System For Better Design User Interface</h1>
